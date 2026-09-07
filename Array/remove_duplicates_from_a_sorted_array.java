178471dc1b350c1d71db80b59b6f2a01ad3ce5af
kabish <kabishvimala@gmail.com>
2026-09-07 19:46:00 +0530

    Add method to remove duplicates from sorted array
    Implemented a method to remove duplicates from a sorted array and print the unique elements.

---
+++ Array/remove_duplicates_from_a_sorted_array.java
@@ -0,0 +1,18 @@
+public class remove_duplicates_from_a_sorted_array {
+    public static void main(String[] args) {
+        int arr[] = {2, 2, 3, 3, 4, 6, 6};
+
+        int rd = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[rd] != arr[i]) {
+                rd++;
+                arr[rd] = arr[i];
+            }
+        }
+
+        for (int i = 0; i <= rd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
