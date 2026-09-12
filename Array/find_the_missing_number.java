b28e68d73edc74a86918a26a4aea4a6272be5327
kabish <kabishvimala@gmail.com>
2026-09-12 07:14:52 +0530

    Add Java program to find the missing number

---
+++ Array/find_the_missing_number.java
@@ -0,0 +1,16 @@
+public class find_the_missing_number {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 5};
+        int n = arr.length + 1;
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missing = expectedSum - actualSum;
+        System.out.println(missing);
+    }
+}
