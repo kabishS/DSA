f5e0facfa5c0e496af16ef78d0995bf8ac79fc08
kabish <kabishvimala@gmail.com>
2026-09-14 18:48:20 +0530

    Refactor to find second smallest and largest elements
    Refactored the logic to find the second smallest and second largest elements in an array. Added handling for both lowest and highest values.

--- Array/find_the_second_smallest_element.java
+++ Array/find_the_second_smallest_element.java
@@ -1,19 +1,33 @@
 public class find_the_second_smallest_element {
     public static void main(String[] args) {
         int arr[] = {5,2,9,1,7};
-        int ss = arr[0];
 
-        for(int i=0; i<arr.length; i++){
-            if(ss >= arr[i]){
-                ss = arr[i];
+        int lowest = Integer.MAX_VALUE;
+        int secondLowest = Integer.MAX_VALUE;
+        
+        int highest = Integer.MIN_VALUE;
+        int secondHighest = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            // Find lowest and second lowest
+            if (num < lowest) {
+                secondLowest = lowest;
+                lowest = num;
+            } else if (num < secondLowest && num != lowest) {
+                secondLowest = num;
+            }
+            
+            // Find highest and second highest
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            } else if (num > secondHighest && num != highest) {
+                secondHighest = num;
             }
         }
-
-        for(int i=0; i<arr.length; i++){
-            if(ss <= arr[i]){
-                ss = arr[i];
-                System.out.println(ss);
-            }
-        } 
+        
+        System.out.println("Second Lowest: " + secondLowest);
+        System.out.println("Second Highest: " + secondHighest);
+        
     }
 }
