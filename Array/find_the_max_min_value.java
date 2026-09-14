e3bd44c87946bdd49d59b32c6f11ed3c70400b72
kabish <kabishvimala@gmail.com>
2026-09-14 14:32:24 +0530

    Add files via upload

---
+++ Array/find_the_max_min_value.java
@@ -0,0 +1,22 @@
+public class find_the_max_min_value {
+    public static void main(String[] args) {
+        int arr[] = {5,2,9,1,7};
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=0; i<arr.length; i++){
+            if(max <= arr[i]){
+                max = arr[i];
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(min >= arr[i]){
+                min = arr[i];
+            }
+        }
+
+        System.out.println("max "+ max);
+        System.out.println("min " + min);
+    }
+}
